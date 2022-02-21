a0549551a0c7338f1d3541f900258a6f7f1ac15c
esme-marie <esmemarie.sultan@gmail.com>
2022-02-22 00:13:45 +0800

    Update auth0 config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-require('dotenv').config()
+require("dotenv").config();
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = {}
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-// }
+  return {
+    env: {
+      REDIRECT_URI: isDev
+        ? "http://localhost:3000/api/callback"
+        : "https://sgweatherforecast.vercel.app/api/callback",
+      POST_LOGOUT_REDIRECT_URI: isDev
+        ? "http://localhost:3000/"
+        : "https://sgweatherforecast.vercel.app/",
+      AUTH0_SCOPE: "openid profile",
+      SERVER_URL: isDev
+        ? "http://localhost:3000"
+        : "https://sgweatherforecast.vercel.app/",
+    },
+  };
+};
